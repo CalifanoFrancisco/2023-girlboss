f5c7941f76e57147c9f5f3956aab1c87b6a62dd7
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-03-09 15:42:26 -0300

    Add files via upload

--- CPP/WFC/WFC.cpp
+++ CPP/WFC/WFC.cpp
@@ -1,1 +1,39 @@
 #include "WFC.h"
+
+wfc::PixelInfo::PixelInfo(unsigned int x_, unsigned int y_, bmp::rgb_t color_):
+    x(x_),y(y_),adjacency(std::vector<bmp::rgb_t>(x_ * y_)), center_color(color_)
+{ }
+
+/*
+wfc::Data::Data(const bmp::Bitmap& bitmap) {
+    m_info.resize(bitmap.width() * bitmap.width());
+    init();
+}*/
+
+void wfc::Data::init() {
+
+}
+
+void wfc::Data::load() {
+    unsigned int area = 2;
+    for (int i = 0; m_info.size(); i++) {
+        m_info[i].center_color = m_bitmap.getPixel(area, area);
+        for (int y = 0; y < m_bitmap.height(); y++) {
+        for (int x = 0; x < m_bitmap.width();  x++) {
+            if (!(
+                x - area < 0         || y - area < 0 ||
+                x > m_bitmap.width() || y > m_bitmap.height()
+            )) {
+                m_info[i].adjacency[y * m_bitmap.width() + x] =
+                m_bitmap.getPixel(x - area, y - area);
+            }
+        }
+        }
+    }
+    bmp::Bitmap out(m_bitmap.width(),m_bitmap.height());
+    for (int y = 0; y < m_bitmap.height();y++){
+    for (int x = 0; x < m_bitmap.width(); x++) {
+        out.setPixel(x,y,m_info[y*m_bitmap.width()+x].center_color);
+    }
+    }
+}
--- CPP/WFC/WFC.h
+++ CPP/WFC/WFC.h
@@ -1,6 +1,34 @@
 #ifndef WFC_IMAGE
 #define WFC_IMAGE
 
+#include <iostream>
+#include <vector>
+
+#include "BMP.h"
+
+namespace wfc {
+
+struct PixelInfo {
+    bmp::rgb_t center_color;
+    unsigned int x, y;
+    std::vector<bmp::rgb_t> adjacency;
+    PixelInfo(unsigned int x_, unsigned int y_, bmp::rgb_t color_);
+};
+
+class Data {
+private:
+    bmp::Bitmap m_bitmap;
+    std::vector<PixelInfo> m_info;
+public:
+    Data (const bmp::Bitmap& bitmap);
+    void init();
+    void load();
+};
+
+
+
+}
 
 
 #endif
+
